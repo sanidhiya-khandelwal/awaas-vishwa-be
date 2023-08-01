3a865a47bbdceb98393f1eb286e531ca3d5c336b
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2023-08-01 13:16:25 +0530

    feat: health route

--- routes/HealthRoute.js
+++ routes/HealthRoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.end('Awaas Vishwa Backed is healthy')
+    res.end('Awaas Vishwa Backend is healthy')
 })
 
 module.exports = router
